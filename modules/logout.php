50025b204a3477ff078653b59dbd1fe17a6b0e01
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-05-21 15:13:46 +0100

    added function to control session

---
+++ modules/logout.php
@@ -0,0 +1,4 @@
+<?php
+require('./sessionControl.php');
+
+logOut();
---
+++ modules/sessionControl.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+
+function authUser()
+{
+    $useremail = $_POST['useremail'];
+    $password = $_POST['password'];
+    validateUser($useremail, $password);
+    // echo $useremail . $password;
+}
+
+
+
+function validateUser($useremail, $password)
+{
+
+    $dbUserEmail = 'alejandroaperez1994g@gmail.com';
+    $dbPassword = password_hash('123', PASSWORD_DEFAULT);
+
+    if ($useremail === $dbUserEmail && password_verify($password, $dbPassword)) {
+        $_SESSION['useremail'] = $useremail;
+        $_SESSION['password'] = $password;
+        header("Location: ../home.php");
+    } else {
+        header("Location: ../index.php?auth-error");
+    }
+}
+
+function logOut()
+{
+    unset($_SESSION);
+
+    // destroy session cookie
+    destroyCookies();
+
+    // destroy the session
+    session_destroy();
+
+    header("Location: ../index.php?logout");
+}
+
+
+function destroyCookies()
+{
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+}
+
+function checkErr($typeError)
+{
+    if (count($typeError) > 0) {
+        $error = '';
+        switch ($typeError) {
+            case isset($typeError['logout']):
+                $error = "logout";
+                break;
+            case isset($typeError['auth-error']):
+                $error =  "auth-error";
+                break;
+            case isset($typeError['permission-error']):
+                $error =  "permission-error";
+                break;
+            default:
+                $error = '';
+        }
+
+        return $error;
+    } else {
+        return "";
+    }
+}

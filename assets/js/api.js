61b95e04ab8fd6f801cfe6d7c5b1dc6fb2e909f7
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-05-22 20:00:21 +0100

    change api url and api_key variable

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,10 @@
 require("dotenv").config({ path: "../../.env" });
 
-const api_key = process.env.IMDB_API_KEY;
+const api_key = process.env.MOVIEDB_API_KEY;
 
 const getBanner = async () => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&with_networks=213&language=en-US`
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}&language=en-US`
   );
   const data = await response.json();
   return data.results[Math.floor(Math.random() * data.results.length - 1)];
